ac074d33027e17b10521525b75b5592774b71cb7
Tasha Stenner <Tasha.Stenner@Nology.local>
2020-04-07 16:21:23 +0100

    fixed-299198-re-renders

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./App.module.scss";
 
 function App() {
+const [allFilms, setAllFilms]= useState([])
+
   const getFilms = (searchTerm) => {
     fetch(`http://www.omdbapi.com/?apikey=6b41f752&s=${searchTerm}`)
       .then((result) => result.json())
       .then((result) => {
-        const films = result.Search;
-        let index = 0;
-        for (index <= 10; index + 1;) {
-          console.log(films[index].Title);
-        }
+       setAllFilms(cleanFilmData(result.Search))
+       console.log(allFilms)
       })
       .catch((err) => console.log(err));
+  };
+
+  const cleanFilmData = films => {
+    return films.map(film => {
+      return { ...film, Title: film.Title, Img: film.Poster, Id: film.imbdID };
+    });
   };
 
   return (
